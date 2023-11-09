4aebebb396b52baec8a778b5e5b2e3e8c7c75327
jay sung <namic123@naver.com>
2023-11-09 12:29:42 +0900

    새 게시글 쓰기 및 Controller, service, mapper, dto, table 생성
    table 만든 쿼리
    CREATE TABLE board(
      id INT PRIMARY KEY AUTO_INCREMENT,
        title VARCHAR(200) NOT NULL ,
        content VARCHAR(1000) NOT NULL ,
        writer VARCHAR(100) NOT NULL,
        inserted DATETIME NOT NULL  DEFAULT NOW()
    );

--- src/main/java/com/example/miniprojectbackend/controller/BoardController.java
+++ src/main/java/com/example/miniprojectbackend/controller/BoardController.java
@@ -1,7 +1,10 @@
 package com.example.miniprojectbackend.controller;
 
+import com.example.miniprojectbackend.domain.Board;
+import com.example.miniprojectbackend.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/board")
 @RequiredArgsConstructor
 public class BoardController {
+    private final BoardService service;
+
     @PostMapping("add")
-    public void add(){
-        System.out.println("BoardController.add");
+    public void add(@RequestBody Board board){  // 요청 본문을 가져옴
+        System.out.println("board = " + board);
+        service.save(board);
     }
 }
