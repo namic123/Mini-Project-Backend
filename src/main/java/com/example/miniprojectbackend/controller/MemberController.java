2e60da56b10036c2c751a57e4fc3010815e71f58
jay sung <namic123@naver.com>
2023-11-10 15:34:21 +0900

    회원가입 정보 검증, 응답코드를 세분화

--- src/main/java/com/example/miniprojectbackend/controller/MemberController.java
+++ src/main/java/com/example/miniprojectbackend/controller/MemberController.java
@@ -12,8 +12,16 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService service;
     @PostMapping("signup")
-    public void signup(@RequestBody Member member){
-        service.add(member);
+    public ResponseEntity signup(@RequestBody Member member){
+        if(service.validate(member)){
+            if(service.add(member)){
+                return ResponseEntity.ok().build();
+            }else {
+                return ResponseEntity.internalServerError().build();
+            }
+        }else {
+            return ResponseEntity.badRequest().build();
+        }
     }
     @GetMapping(value = "check", params = "id")
     public ResponseEntity checkDuplicates(String id){
