c35e319a02896d4b9dc7f6c9a70bf0583bedc3dd
jay sung <namic123@naver.com>
2023-11-14 15:45:37 +0900

    권한 테이블 생성 및, 권한 기능 추가
    CREATE TABLE auth(
        id INT PRIMARY KEY AUTO_INCREMENT,
        memberId VARCHAR(100) NOT NULL REFERENCES member(id),
        name VARCHAR(100) NOT NULL
    );
    
    INSERT INTO auth(memberId, name)
    VALUES ('namic123', 'admin');

---
+++ src/main/java/com/example/miniprojectbackend/domain/Auth.java
@@ -0,0 +1,10 @@
+package com.example.miniprojectbackend.domain;
+
+import lombok.Data;
+
+@Data
+public class Auth {
+    private Integer id;
+    private String memberId;
+    private String name;
+}
